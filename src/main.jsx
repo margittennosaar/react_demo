b98aef649255b1b620eae3130062fd0dd8684e97
Margit Tennosaar <margit.tennosaar@bc.fi>
2025-04-07 16:23:24 +0300

    week1 updates: clean up unused styles and components, update App structure

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,26 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
 
-createRoot(document.getElementById('root')).render(
+const name = "Margit Tennosaar";
+const first_page = 1998;
+
+const Box = () => {
+  return <p>Greetings from Box component</p>;
+};
+
+const App = () => {
+  return (
+    <div>
+      <h1>Hello, my name is {name} </h1>
+      <p>I have coding {2025 - first_page} years </p>
+      <Box />
+    </div>
+  );
+};
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
