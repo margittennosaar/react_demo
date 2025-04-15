bd08f8dc48f95d8d88edba5b4bd9155473600e70
Margit Tennosaar <margit.tennosaar@bc.fi>
2025-04-15 16:04:03 +0300

    add week4 code examples

--- src/Books/BookCard.jsx
+++ src/Books/BookCard.jsx
@@ -7,19 +7,42 @@ const BookCard = ({
   genre,
   inStock,
   isFavorite,
+  onEventHandler,
+  id,
+  onToggleStock,
+  onToggleFavorite,
   ...rest
 }) => {
-  console.log(rest);
+  /*   console.log(rest); */
+  const eventHandler = () => {
+    console.log("You clicked 'Add wishlist' button is clicked");
+  };
 
   return (
     <div className="bookCard">
-      <h2>{title}</h2>
-      <p>{isFavorite ? "♥️" : "♡"}</p>
-      <p>{author}</p>
-      <p>{price}€</p>
-      <p>{genre}</p>
-      <p>{inStock ? "In stock" : "Out of Stock"}</p>
-      {!inStock && <button>Add to Wishlist</button>}
+      <div className="bookCard-header">
+        <p
+          className={inStock ? "stock" : "stock outOf"}
+          onClick={() => onToggleStock(id)}
+        >
+          {inStock ? "In stock" : "Out of Stock"}
+        </p>
+        <h2>{title}</h2>
+        <p className="favIcon" onClick={onToggleFavorite}>
+          {isFavorite ? "♥️" : "♡"}
+        </p>
+        <p>{author}</p>
+        <p className="genre">{genre}</p>
+      </div>
+      <div className="bookCard-content">
+        <div></div>
+        <p className="price">{price}€</p>
+      </div>
+      <div className="bookCard-footer">
+        {/*       <button onClick={() => onEventHandler(id)}>Read more</button> */}
+        <button onClick={onEventHandler}>See more</button>
+        {!inStock && <button onClick={eventHandler}>Add to Wishlist</button>}
+      </div>
     </div>
   );
 };
